b9cd5c5558de746b8f17994179d33fd2f5bd2442
rakaso598 <nexonaddress98@gmail.com>
2025-08-29 12:41:02 +0900

    feat: add "use client" directive to multiple pages for client-side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import DashboardLayout from "../components/DashboardLayout";
 import useSWR from "swr";
--- app/records/[key]/edit/page.tsx
+++ app/records/[key]/edit/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
